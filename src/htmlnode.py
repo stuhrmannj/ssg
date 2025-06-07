ff8c8163ccf58ca44461cd74a601c633677877b5
stuhrmannj <stuhrmannj@hotmail.com>
2025-06-07 10:26:15 -0400

    added textnode to htmlnode function and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+import textnode
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -12,4 +14,44 @@ class HTMLNode:
         return "".join([f' {key}="{value}"' for key, value in self.props.items()])
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode): 
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None or len(self.children) == 0:
+            raise ValueError("ParentNode must have children")
+        children_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == textnode.TextType.NORMAL:
+        return LeafNode(tag=None, value=text_node.text, props=None)
+    elif text_node.text_type == textnode.TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text, props=None)
+    elif text_node.text_type == textnode.TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text, props=None)
+    elif text_node.text_type == textnode.TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text, props=None)
+    elif text_node.text_type == textnode.TextType.LINK:
+        return LeafNode(tag="a", value=text_node.text, props={"href":text_node.url})
+    elif text_node.text_type == textnode.TextType.IMAGE:
+        return LeafNode(tag="img", value="", props={"src":text_node.url, "alt":text_node.text})
+    else:
+        raise ValueError
+
+    
+
     
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,24 +1,24 @@
 import unittest
-
-from htmlnode import HTMLNode
+import htmlnode
+import textnode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_empty_props(self):
         # Test with None props
-        node = HTMLNode("div", "text", None, None)
+        node = htmlnode.HTMLNode("div", "text", None, None)
         self.assertEqual(node.props_to_html(), "")
         
         # Test with empty dict props
-        node = HTMLNode("div", "text", None, {})
+        node = htmlnode.HTMLNode("div", "text", None, {})
         self.assertEqual(node.props_to_html(), "")
 
     def test_props_to_html_with_props(self):
         # Test with single prop
-        node = HTMLNode("a", "Click me", None, {"href": "https://example.com"})
+        node = htmlnode.HTMLNode("a", "Click me", None, {"href": "https://example.com"})
         self.assertEqual(node.props_to_html(), ' href="https://example.com"')
         
         # Test with multiple props
-        node = HTMLNode(
+        node = htmlnode.HTMLNode(
             "a",
             "Click me",
             None,
@@ -39,7 +39,146 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_repr_method(self):
         # Test that __repr__ returns a string
-        node = HTMLNode("div", "text", None, {"class": "container"})
+        node = htmlnode.HTMLNode("div", "text", None, {"class": "container"})
         repr_str = repr(node)
         self.assertIsInstance(repr_str, str)
         # Verify
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_no_value(self):
+        # Test that ValueError is raised when value is None
+        node = htmlnode.LeafNode("div", None, {"class": "container"})
+        with self.assertRaises(ValueError):
+            node.to_html() 
+
+    def test_to_html_with_value(self):
+        # Test that to_html returns the correct HTML string
+        node = htmlnode.LeafNode("div", "text", {"class": "container"})
+        self.assertEqual(node.to_html(), '<div class="container">text</div>') 
+
+    def test_to_html_no_tag(self):
+        # Test that to_html returns the value when tag is None
+        node = htmlnode.LeafNode(None, "text", {"class": "container"})
+        self.assertEqual(node.to_html(), "text") 
+
+    def test_leaf_to_html_p(self):
+        # Test that to_html returns the correct HTML string for a paragraph
+        node = htmlnode.LeafNode("p", "text", {"class": "container"})
+        self.assertEqual(node.to_html(), '<p class="container">text</p>')
+
+    def test_leaf_to_html_b(self):
+        # Test that to_html returns the correct HTML string for a bold text
+        node = htmlnode.LeafNode("b", "text", {"class": "container"})
+        self.assertEqual(node.to_html(), '<b class="container">text</b>')
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_no_tag(self):
+        # Test that ValueError is raised when tag is None
+        node = htmlnode.ParentNode(None, [htmlnode.LeafNode("div", "text")], {"class": "container"})
+        with self.assertRaises(ValueError):
+            node.to_html() 
+
+    def test_to_html_no_children(self):
+        # Test that ValueError is raised when children is None
+        node = htmlnode.ParentNode("div", None, {"class": "container"})
+        with self.assertRaises(ValueError):
+            node.to_html() 
+
+    def test_to_html_empty_children(self):
+        # Test that ValueError is raised when children is empty
+        node = htmlnode.ParentNode("div", [], {"class": "container"})
+        with self.assertRaises(ValueError):
+            node.to_html() 
+
+    def test_to_html_with_children(self):
+        # Test that to_html returns the correct HTML string
+        child1 = htmlnode.LeafNode("p", "text1", {"class": "child"})
+        child2 = htmlnode.LeafNode("p", "text2", {"class": "child"})
+        node = htmlnode.ParentNode("div", [child1, child2], {"class": "container"})
+        expected_html = '<div class="container"><p class="child">text1</p><p class="child">text2</p></div>'
+        self.assertEqual(node.to_html(), expected_html)
+    
+    def test_to_html_with_children(self):
+        child_node = htmlnode.LeafNode("span", "child")
+        parent_node = htmlnode.ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = htmlnode.LeafNode("b", "grandchild")
+        child_node = htmlnode.ParentNode("span", [grandchild_node])
+        parent_node = htmlnode.ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(),"<div><span><b>grandchild</b></span></div>")
+
+    def test_to_html_with_grandchildren_and_props(self):
+        grandchild_node = htmlnode.LeafNode("b", "grandchild", {"class": "grandchild"})
+        child_node =  htmlnode.ParentNode("span", [grandchild_node], {"class": "child"})
+        parent_node =  htmlnode.ParentNode("div", [child_node], {"class": "parent"})
+        expected_html = '<div class="parent"><span class="child"><b class="grandchild">grandchild</b></span></div>'
+        self.assertEqual(parent_node.to_html(), expected_html)
+    
+    def test_multiple_children(self):
+        child1 =  htmlnode.LeafNode("p", "text1", {"class": "child"})
+        child2 =  htmlnode.LeafNode("p", "text2", {"class": "child"})
+        node =  htmlnode.ParentNode("div", [child1, child2], {"class": "container"})
+        expected_html = '<div class="container"><p class="child">text1</p><p class="child">text2</p></div>'
+        self.assertEqual(node.to_html(), expected_html)
+
+    def test_parent_to_html_div(self):
+        # Test that to_html returns the correct HTML string for a div
+        child_node =  htmlnode.LeafNode("span", "child", {"class": "child"})
+        parent_node =  htmlnode.ParentNode("div", [child_node], {"class": "parent"})
+        self.assertEqual(parent_node.to_html(), '<div class="parent"><span class="child">child</span></div>')
+
+    def test_parent_to_html_span(self):
+        # Test that to_html returns the correct HTML string for a span
+        child_node =  htmlnode.LeafNode("b", "child", {"class": "child"})
+        parent_node =  htmlnode.ParentNode("span", [child_node], {"class": "parent"})
+        self.assertEqual(parent_node.to_html(), '<span class="parent"><b class="child">child</b></span>')
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    # Test that the text texttype is properly converted to html
+    def test_text(self):
+        node =  textnode.TextNode("This is a text node", textnode.TextType.NORMAL)
+        html_node = htmlnode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node") 
+    # Test that the bold texttype is properly converted to html
+    def test_bold(self):
+        node =  textnode.TextNode("This is a bold node", textnode.TextType.BOLD)
+        html_node = htmlnode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node") 
+    # Test that the italic texttype is properly converted to html
+    def test_italic(self):
+        node =  textnode.TextNode("This is an italic node", textnode.TextType.ITALIC)
+        html_node = htmlnode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node") 
+    # Test that the code texttype is properly converted to html
+    def test_code(self):
+        node =  textnode.TextNode("This is a code node", textnode.TextType.CODE)
+        html_node = htmlnode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node") 
+    # Test that the link texttype is properly converted to html
+    def test_link(self):
+        test_link_url = "https://www.google.com"
+        node =  textnode.TextNode("This is a link node", textnode.TextType.LINK, test_link_url)
+        html_node = htmlnode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node") 
+        self.assertEqual(html_node.props, {"href":test_link_url})
+    # Test that the image texttype is properly converted to html
+    def test_image(self):
+        test_image_url = "https://www.image_site.com"
+        node =  textnode.TextNode("This is an image node", textnode.TextType.IMAGE, test_image_url)
+        html_node = htmlnode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "") 
+        self.assertEqual(html_node.props, {"src":test_image_url, "alt":"This is an image node"})
+    # Test an incorrect texttype
+    def test_unknown_type(self):
+        node =  textnode.TextNode("This is a text node", textnode.TextType.NORMAL)
+        node.text_type = "unknown"
+        with self.assertRaises(ValueError):
+            htmlnode.text_node_to_html_node(node)
