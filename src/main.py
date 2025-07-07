e9c46d407cb3de09c74e165f0e4a160779f54c45
stuhrmannj <stuhrmannj@hotmail.com>
2025-07-07 14:39:46 -0400

    added static to public copy function to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_recursive(src, dst):
+    for entry in os.listdir(src):
+        src_entry_path = os.path.join(src, entry)
+        dst_entry_path = os.path.join(dst, entry)
+        print(dst_entry_path)
+        if os.path.isfile(src_entry_path):
+            shutil.copy(src_entry_path, dst_entry_path)
+        elif os.path.isdir(src_entry_path):
+            os.mkdir(dst_entry_path)
+            copy_recursive(src_entry_path,dst_entry_path)
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    # identifies the directory this file is run from
+    src_dir = os.path.dirname(os.path.abspath(__file__))
 
-    print(node)
+    # identifies the directory one level up, where public should be
+    public_dir = os.path.join(src_dir, "..", "public")
+
+    # identifies the static directory, also one level up
+    static_dir = os.path.join(src_dir, "..", "static")
+
+    # check if public exists, and if it does, delete it
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+
+    # create a new public directory
+    os.mkdir(public_dir)
+
+    # call the copy function to copy entire static directory into the public directory
+    copy_recursive(static_dir, public_dir)
 
 if __name__ == "__main__":
     main()
